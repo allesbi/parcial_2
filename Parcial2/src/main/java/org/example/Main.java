e33de1864fed8d92099e79304bb63c22d8c926e0
Alejandro Restrepo <alejandrorestrepocorrales@gmail.com>
2026-09-10 11:01:10 -0500

    fase_1 codigo
    todo el codigo de la fase 1

---
+++ Parcial2/src/main/java/org/example/Main.java
@@ -0,0 +1,41 @@
+package org.example;
+import org.example.Model.Pokemon;
+import org.example.Model.LineaEvolutiva;
+import org.example.Util.GeneradorHorda;
+
+public class Main {
+    public static void main(String[] args) {
+        Pokemon charmander = new Pokemon("Charmander", 39, 52, 43, 42, 1500);
+        Pokemon charmeleon = new Pokemon("Charmeleon", 58, 64, 58, 63, 5000);
+        Pokemon charizard = new Pokemon("Charizard", 78, 84, 78, 75, -1);
+
+        charmander.setSiguienteEvolucion(charmeleon);
+        charmeleon.setSiguienteEvolucion(charizard);
+
+        LineaEvolutiva miPokemon = new LineaEvolutiva(charmander);
+
+        //Prueba unitaria solo con rattata
+        Pokemon enemigos[] = new Pokemon[1];
+        Pokemon rattata = new Pokemon("rattata", 10, 10, 10, 100, 1000);
+
+        enemigos[0] = rattata;
+        LineaEvolutiva.iniciarEntrenamientoMasivo(miPokemon, enemigos);
+
+        //Prueba de el ejercito de catterpies
+        Pokemon[] hordaCatterpie = new Pokemon[100000];
+
+        for (int i = 0; i < hordaCatterpie.length; i++) {
+
+            hordaCatterpie[i] = new Pokemon("catterpie", 45, 30, 35, 20, 50);
+
+        }
+
+
+        //Despues de comprobar que funcionaban los catterpies hice(con la ayuda de gemini) un aleatorizador de los pokemones en la horda, de todas formas sigue dando 50 de xp por combate entonces igual sigue evolucionando a los 30 y a los 100
+        Pokemon[] hordaSuprema = GeneradorHorda.generarHordaAleatoria(100000);
+
+        LineaEvolutiva.iniciarEntrenamientoMasivo(miPokemon, hordaSuprema);
+    }
+}
+
+
